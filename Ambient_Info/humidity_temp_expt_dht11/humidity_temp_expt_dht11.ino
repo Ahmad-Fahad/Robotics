9aab55cf909ef5d8c3516a07f1ed0a918ea8bd0e
Ahmad-Fahad <ahmadsmfahad@gmail.com>
2022-02-09 13:10:11 +0600

    Project_Work

--- Ambient_Info/humidity_temp_expt_dht11/humidity_temp_expt_dht11.ino
+++ Ambient_Info/humidity_temp_expt_dht11/humidity_temp_expt_dht11.ino
@@ -1,9 +1,23 @@
+#include<DHT.h>
+#define DHTPIN A1
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-  // put your setup code here, to run once:
-
+  dht.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.println();
+  Serial.print("Humidity = ");
+  Serial.print(h);
+  Serial.println("%");
+  Serial.println();
+  Serial.print("Temperature = ");
+  Serial.print(t);
+  Serial.println(" *C");
+  delay(5000);
 
 }
